b88188be6a34e8a0cd1bb74b17bd920e8dc8a184
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-09-17 11:25:05 +0200

    fix router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '*path', to: 'home#index', via: :all
+  get '*path', to: "home#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 
   namespace :api do
     namespace :v1 do
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
         post :estimate, on: :collection
       end
       get :logged_in, to: 'accounts#logged_in'
-      devise_for :users, module: "api/v1/authg", defaults: { format: :json }
+      devise_for :users, module: "api/v1/auth", defaults: { format: :json }
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
