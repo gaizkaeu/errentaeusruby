e71d0a8da3e23461ca6457125bd0c99a0fbd2a46
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-09-24 11:52:14 +0200

    esbuild

--- app/javascript/src/routes.tsx
+++ app/javascript/src/routes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App'
