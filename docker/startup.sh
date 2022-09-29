6935255ad84dc14afb50534377d627b16886f4e4
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-09-29 17:59:38 +0200

    startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -7,7 +7,7 @@ echo "----------------"
 
 # Create or migrate database
 echo "Preparing database..."
-bin/rails db:prepare
+bin/rails db:reset
 echo "Database is ready!"
 
 bundle exec puma -C config/puma.rb
