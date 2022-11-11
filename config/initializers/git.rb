b6a0f27f515341979c88c78429e8b5ba9c7198fd
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-11-12 00:12:19 +0100

    git info, titles and theme

---
+++ config/initializers/git.rb
@@ -0,0 +1,7 @@
+Rails.configuration.x.git.commit_version =
+  ENV.fetch('COMMIT_VERSION') { `git describe --always`.chomp }
+
+Rails.configuration.x.git.commit_time =
+  Time.zone.parse(ENV.fetch('COMMIT_TIME') { `git show -s --format=%cI` })
+
+Rails.configuration.x.git.home = 'https://github.com/gaizkaurd/errentaeusruby'
