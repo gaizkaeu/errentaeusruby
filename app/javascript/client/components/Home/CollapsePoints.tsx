b5f681c01c606ed8bb2d0566d512148542b319e5
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-11-16 12:12:18 +0100

    some more tests

--- app/javascript/client/components/Home/CollapsePoints.tsx
+++ app/javascript/client/components/Home/CollapsePoints.tsx
@@ -2,7 +2,7 @@ import { StarIcon } from "@heroicons/react/24/outline";
 import { Grid, Col, Collapse, Row, Text, Link } from "@nextui-org/react";
 import { useState } from "react";
 import { FeatureItem } from "../Card-Grid/styles";
-import { Box, Section, Subtitle, Title } from "../Primitives";
+import { Section, Subtitle, Title } from "../Primitives";
 
 const items = [
   {
@@ -35,20 +35,33 @@ const CollapsePoints = () => {
   return (
     <div className="py-20 sm:py-24 lg:py-28">
       <Section css={{ position: "relative" }}>
-        <Box
-          css={{
-            position: "absolute",
-            top: "-50%",
-            left: "-10%",
-            zIndex: "-$1",
-            "@xsMax": {
-              top: "-35%",
-              left: "-90%",
-            },
-          }}
-        >
-          <img alt="theming background" src="/collapsepointsbg.svg" />
-        </Box>
+        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+          <svg
+            className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+            viewBox="0 0 1155 678"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+              fillOpacity=".3"
+              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+            />
+            <defs>
+              <linearGradient
+                id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                x1="1155.49"
+                x2="-78.208"
+                y1=".177"
+                y2="474.645"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#9089FC" />
+                <stop offset={1} stopColor="#FF80B5" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
         <Row justify="flex-start">
           <Text className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
             ¿Qué nos
