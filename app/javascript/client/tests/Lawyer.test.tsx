7546abaaceb1a6fd63837281fe48e82062ff80c9
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-12-03 23:48:15 +0100

    yarn test fix

--- app/javascript/client/tests/Lawyer.test.tsx
+++ app/javascript/client/tests/Lawyer.test.tsx
@@ -1,16 +1,17 @@
 import renderer from "react-test-renderer";
-import LawyerAvatar from "../components/Lawyer/Lawyer";
+import { UserAvatar } from "../components/Lawyer/Lawyer";
 import { IUser } from "../storage/types";
 
 it("Lawyer::Avatar: show correct data", () => {
   const user: IUser = {
     id: "0",
+    confirmed: false,
     email: "gaizkaurdangarin@gmail.com",
     first_name: "Gaizka",
     last_name: "Urdangarin",
     account_type: "user",
   };
-  const component = renderer.create(<LawyerAvatar lawyer={user} size="sm" />);
+  const component = renderer.create(<UserAvatar user={user} size="sm" />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
