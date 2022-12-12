eae35d40d306ff4ffb242c510fa252331bd64bb6
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-12-12 11:34:14 +0100

    rubocop

--- config/initializers/cossorigin.rb
+++ config/initializers/cossorigin.rb
@@ -2,6 +2,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://errenta.eus'
     resource '*', headers: :any, methods: %i[get post patch put], credentials: true
-    
   end
 end
