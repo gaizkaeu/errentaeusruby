e1c8001d85a9a1c1c8dd7938891803103bb53256
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-12-12 12:04:01 +0100

    disable change headers on each request

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,6 +5,8 @@ DeviseTokenAuth.setup do |config|
   config.cookie_enabled = true
   config.cookie_name = 'errenta_auth'
 
+  config.change_headers_on_each_request = false
+
   config.cookie_attributes = { secure: true, httponly: true, same_site: :strict, domain: ENV.fetch('APP_HOST_FRONTEND_DOMAIN', 'https://errenta.eus') }
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
