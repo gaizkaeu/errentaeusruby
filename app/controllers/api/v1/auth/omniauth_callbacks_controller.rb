bdbcc38d92097510d519c6946f4f9a8cf9c0ae88
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-12-14 17:29:30 +0100

    rubocop

--- app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     module Auth
-      class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+      class OmniauthCallbacksController < Api::V1::ApiBaseController
         def google_one_tap
           payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: '321891045066-2it03nhng83jm5b40dha8iac15mpej4s.apps.googleusercontent.com')
 
