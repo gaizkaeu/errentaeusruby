416a3df52edc63d181ac3936687f5ac6537c6700
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-12-18 18:07:46 +0100

    rubocop was doing the save bang...

--- packages/tax_incomes/app/public/api/v1/services/create_tax_service.rb
+++ packages/tax_incomes/app/public/api/v1/services/create_tax_service.rb
@@ -6,8 +6,9 @@ module Api::V1::Services
       tax_record = Api::V1::TaxIncomeRecord.new(tax_income_params)
       tax = Api::V1::TaxIncome.new(tax_record.attributes.symbolize_keys!)
       authorize_with current_account, tax, :create?
-
+      # rubocop:disable Rails/SaveBang
       tax_record.save
+      # rubocop:enable Rails/SaveBang
       tax = Api::V1::TaxIncome.new(tax_record.attributes.symbolize_keys!)
       tax.instance_variable_set(:@errors, tax_record.errors)
       tax
