b5ed766493c8deb65c56d8cf53601c8c552466a5
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2023-01-17 17:51:13 +0100

    bullet initializer

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,2 +1,4 @@
-Bullet.add_safelist type: :unused_eager_loading, class_name: 'Api::V1::UserRecord', association: :account
-Bullet.add_safelist type: :unused_eager_loading, class_name: 'Api::V1::TaxIncome', association: :appointment
+if Rails.env.development? || Rails.env.test?
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Api::V1::UserRecord', association: :account
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Api::V1::TaxIncome', association: :appointment
+end
