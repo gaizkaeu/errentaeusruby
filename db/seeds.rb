fab42323c96891e5debc2a8d5daebac2477fa5d2
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2023-01-21 18:07:42 +0100

    rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ admin = Account.create!(email: 'gaizkaurdangarin@gmail.com', status: 2)
 acc_gestion = Account.create!(email: 'gestion@elizaasesores.com', status: 2)
 acc_lawyer = Account.create!(email: 'carolina@elizaasesores.com', status: 2)
 
-admin_user = Api::V1::Repositories::UserRepository.add({ first_name: 'Gaizka', last_name: 'Urdangarin', account_type: :admin, account_id: admin.id }, raise_error: true)
+Api::V1::Repositories::UserRepository.add({ first_name: 'Gaizka', last_name: 'Urdangarin', account_type: :admin, account_id: admin.id }, raise_error: true)
 owner = Api::V1::Repositories::UserRepository.add({ first_name: 'Eliza', last_name: 'Asesores', account_type: :org_manage, account_id: acc_gestion.id }, raise_error: true)
 law = Api::V1::Repositories::UserRepository.add({ first_name: 'Carolina', last_name: 'Elizagarate', account_type: :lawyer, account_id: acc_lawyer.id }, raise_error: true)
 org = Api::V1::Repositories::OrganizationRepository.add({ name: 'Eliza Asesores', phone: '1234567890', email: 'contacto@elizaasesores.com', website: 'https://www.elizaasesores.com', description: 'Eliza Asesores', latitude: 42.84, longitude: -2.67, owner_id: owner.id }, raise_error: true)
