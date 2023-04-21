e8989628a0c725f7bd0051ad368fc7ba54dcc25e
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2023-04-21 19:18:22 +0200

    add some test data

--- packages/calculations/api/app/contollers/api/v1/organizations/calculators_controller.rb
+++ packages/calculations/api/app/contollers/api/v1/organizations/calculators_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V1::Organizations::CalculatorsController < ApplicationController
   def index
-    calc = Api::V1::Calculator.where(organization_id: params[:organization_id])
+    calc = Api::V1::Calculator.where(organization_id: params[:organization_id]).includes([:calculation_topic])
 
     render json: Api::V1::Serializers::CalculatorSerializer.new(calc)
   end
