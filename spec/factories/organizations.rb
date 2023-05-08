ebb6035c2f8a2a49293b1c3cb0a561c470eb3d89
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2023-05-08 13:33:28 +0200

    back rodatuh

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -6,6 +6,39 @@ FactoryBot.define do
     website { Faker::Internet.url }
     description { Faker::Lorem.paragraph }
 
+    open_close_hours {
+      {
+        monday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        tuesday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        wednesday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        thursday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        friday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        saturday: {
+          open: '09:00',
+          close: '17:00'
+        },
+        sunday: {
+          open: '09:00',
+          close: '17:00'
+        }
+      }
+    }
+
     # Define a trait to create memberships for the organization
     trait :with_memberships do
       transient do
